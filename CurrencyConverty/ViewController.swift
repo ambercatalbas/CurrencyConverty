6a7028af6becf1eec8c04f28524084c8ee0b8ca8
sfctl <catalbasifa@gmail.com>
2021-01-26 15:19:58 +0300

    Initial Commit

---
+++ CurrencyConverty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverty
+//
+//  Created by Yasemin YEL on 26.01.2021.
+//  Copyright © 2021 Sifa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
